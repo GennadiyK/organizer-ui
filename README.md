c4aa363db1491b8a22a18825774ad9e21e59c713
hkalinouski <7742589@gmail.com>
2018-01-24 16:51:51 +0300

    http

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TodoUi
+# Organiser Ui
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'app';
+  user = {};
+  constructor( private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+      this.http.get('http://localhost:3000/user/5a6750d5bcf2a622c87c5eff').subscribe(data => {
+        this.user = data;
+      });
+  }
 }
