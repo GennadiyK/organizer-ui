156069267aa0da399d6515e89976bcb0a5ede3eb
hkalinouski <7742589@gmail.com>
2018-01-29 16:10:35 +0300

    navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {AppHeaderComponent} from './shared/header/header.component';
+import {AppMainNavComponent} from './shared/main-nav/main-nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppMainNavComponent
   ],
   imports: [
     BrowserModule,
